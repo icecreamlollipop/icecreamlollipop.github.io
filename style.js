305dfbc3694260b6e860a280293c79185463a796
icecreamlollipop <nicole.dong@gmail.com>
2020-07-29 20:48:18 -0600

    changing the link

--- style.js
+++ style.js
@@ -3,7 +3,7 @@ function changeContent() {
     mydiv.innerHTML = "";
     var aTag = document.createElement('a');
     // TODO: change this to the correct html -- the one that's not local
-    aTag.setAttribute('href',"C:/Users/ThinkPad/Documents/2_Icecream_Programs/icecreamlollipop.github.io/EarthTimeline/EarthTimeline.html");
+    aTag.setAttribute('href',"icecreamlollipop.github.io/EarthTimeline.html");
     aTag.innerText = "Timeline of Earth";
     mydiv.appendChild(aTag);
 }
