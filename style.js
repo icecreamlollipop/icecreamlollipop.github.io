901f206ccfab49d1354c837627b0a774e44b43f2
icecreamlollipop <nicole.dong@gmail.com>
2020-07-29 20:47:13 -0600

    earth timeline project

--- style.js
+++ style.js
@@ -1,3 +1,9 @@
 function changeContent() {
-    document.getElementById('mainContent').innerHTML = 'your content';
+    var mydiv = document.getElementById("mainContent");
+    mydiv.innerHTML = "";
+    var aTag = document.createElement('a');
+    // TODO: change this to the correct html -- the one that's not local
+    aTag.setAttribute('href',"C:/Users/ThinkPad/Documents/2_Icecream_Programs/icecreamlollipop.github.io/EarthTimeline/EarthTimeline.html");
+    aTag.innerText = "Timeline of Earth";
+    mydiv.appendChild(aTag);
 }
