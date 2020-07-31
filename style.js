2e2a1df9ce4dbfeb33eef55b19d0e1f5907a9010
icecreamlollipop <nicole.dong@gmail.com>
2020-07-30 18:38:06 -0600

    created blocks in timeline

--- style.js
+++ style.js
@@ -3,7 +3,7 @@ function changeContent() {
     mydiv.innerHTML = "";
     var aTag = document.createElement('a');
     // TODO: change this to the correct html -- the one that's not local
-    aTag.setAttribute('href',"icecreamlollipop.github.io/EarthTimeline.html");
+    aTag.setAttribute('href',"https://icecreamlollipop.github.io/EarthTimeline.html");
     aTag.innerText = "Timeline of Earth";
     mydiv.appendChild(aTag);
 }
