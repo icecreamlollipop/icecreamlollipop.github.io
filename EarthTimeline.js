349da0220d28944e2460bd1b9bebd3323d20306a
icecreamlollipop <nicole.dong@gmail.com>
2020-07-31 18:43:18 -0600

    included info on the eons

---
+++ EarthTimeline.js
@@ -0,0 +1,130 @@
+function hadeanChange() {
+    var infoDivTitle = document.getElementById("infoDivTitle");
+    var infoDivContent = document.getElementById("infoDivContent");
+    infoDivTitle.innerHTML = "Hadean Eon"
+    infoDivTime.innerHTML = "(4500 to 4000 million years ago)";
+    infoDivContent.innerHTML = "It's debateable whether life arose in the Hadean Eon or later. Chemical fingerprints in zircon rocks imply that there might have been life near the end of this eon - but we aren't sure of it. If life had existed during the Hadean, it would have been too small and too simple to leave behind fossils. The earliest life forms were probably based on RNA rather than DNA, and lived in undersea alkaline vents.";
+}
+
+function archeanChange(){
+    var infoDivTitle = document.getElementById("infoDivTitle");
+    var infoDivContent = document.getElementById("infoDivContent");
+    infoDivTitle.innerHTML = "Archean Eon"
+    infoDivTime.innerHTML = "(4000 to 2500 million years ago)";
+    infoDivContent.innerHTML = "At some point, a common ancestor splits into two groups of life: bacteria and archaea. Viruses were also present during this eon, but their origin is uncertain. Eventually, cyanobacteria learn how to photosynthesize, producing oxygen as a waste product. This slowly increased the amount of oxygen in Earth's atmosphere, paving the way for more complex organisms in the future. It's interesting to note that very little evolution occurred for such a long period of time - the Archean lasted 1500 million years, yet we are still a long way from finding animal life.";
+}
+
+function proterozoicChange(){
+    var infoDivTitle = document.getElementById("infoDivTitle");
+    var infoDivContent = document.getElementById("infoDivContent");
+    infoDivTitle.innerHTML = "Proterozoic Eon"
+    infoDivTime.innerHTML = "(2500 to 541 million years ago)";
+    infoDivContent.innerHTML = "Thanks to the increase in oxygen, eukaryotes are able to evolve. Some microbes manage to make their way onto land. The first fungi and algae appear, as well as the first protozoa - thought to be the ancestors of all animals. Yet, it's only near the very end of this eon that multicellular animals like jellyfish, worms, sponges, corals, and anemones appear. So far, evolution has been working at an extremely slow pace. Billions of years have passed, yet animal life  has only just started to appear.";
+}
+
+function phanerozoicChange(){
+  var infoDivTitle = document.getElementById("infoDivTitle");
+  var infoDivContent = document.getElementById("infoDivContent");
+  infoDivTitle.innerHTML = "Phanerozoic Eon"
+  infoDivTime.innerHTML = "(541 million years ago to present day)";
+  infoDivContent.innerHTML = "This is the eon where the majority of evolution occurs. This eon starts with the Cambrian Explosion, a sudden diversification of life that occurred 541 million years ago. We will also see that numerous devastating extinction events occurred throughout this eon. The Phanerozoic Eon is divided into eras, which are in turn divided into periods. The previous eons were also divided in similar ways; however, very little evolution occurred in each division, so they were not included on this timeline.";
+}
+
+function paleozoicChange(){
+  var infoDivTitle = document.getElementById("infoDivTitle");
+  var infoDivContent = document.getElementById("infoDivContent");
+  infoDivTitle.innerHTML = "Paleozoic Era"
+  infoDivTime.innerHTML = "(541 to 252 years million years ago)";
+  infoDivContent.innerHTML = "This era starts with the Cambrian explosion, and ends with the largest extinction event in Earth's history (so far) - The Great Dying. In the time between these events, life diversified a lot. Most of the divisions of life we know today, evolved during this time. For instance, insects, vertebrates, and so on.";
+}
+
+function mesozoicChange(){
+  var infoDivTitle = document.getElementById("infoDivTitle");
+  var infoDivContent = document.getElementById("infoDivContent");
+  infoDivTitle.innerHTML = "Mesozoic Era"
+  infoDivTime.innerHTML = "(252 to 66 million years ago)";
+  infoDivContent.innerHTML = "This was the age of dinosaurs. If they hadn't gone extinct, would mammals ever dominate the Earth and evolve into humans? They lived well for millions of years.";
+}
+
+function cenozoicChange(){
+  var infoDivTitle = document.getElementById("infoDivTitle");
+  var infoDivContent = document.getElementById("infoDivContent");
+  infoDivTitle.innerHTML = "Cenozoic Era"
+  infoDivTime.innerHTML = "(66 million years ago to present day)";
+  infoDivContent.innerHTML = "With the extinction of the dinosaurs, an opportunity arises for another type of life to dominate the planet: mammals. Dinosaurs did not completely go extinct, however. Their descendants are birds, which are still the most numerous type of animal on Earth. Perhaps we are still living in the age of dinosaurs.";
+}
+
+
+
+function cambrianChange(){
+  var infoDivTitle = document.getElementById("infoDivTitle");
+  var infoDivContent = document.getElementById("infoDivContent");
+  infoDivTitle.innerHTML = "Cambrian Period"
+  infoDivTime.innerHTML = "(541 to 485 million years ago)";
+  infoDivContent.innerHTML = "Cambrian Explosion and diversification of life. Mostly arthropods. Most life lived near the sea floor. Vertebrates evolved. There were fish, trilobites, and crustaceans. Everything was still in the sea.";
+}
+
+function ordovicianChange(){
+  var infoDivTitle = document.getElementById("infoDivTitle");
+  var infoDivContent = document.getElementById("infoDivContent");
+  infoDivTitle.innerHTML = "Ordovician Period"
+  infoDivTime.innerHTML = "(485 to 444 million years ago)";
+  infoDivContent.innerHTML = "Oceanic life continues to evolve. Sharks, crabs, and starfish appear. Plants and arthropods start to go to land. Ends with an extinction.";
+}
+
+function silurianChange(){
+  var infoDivTitle = document.getElementById("infoDivTitle");
+  var infoDivContent = document.getElementById("infoDivContent");
+  infoDivTitle.innerHTML = "Silurian Period"
+  infoDivTime.innerHTML = "(444 to 419 million years ago)";
+  infoDivContent.innerHTML = "Life starts to appear on land.";
+}
+
+function devonianChange(){
+  var infoDivTitle = document.getElementById("infoDivTitle");
+  var infoDivContent = document.getElementById("infoDivContent");
+  infoDivTitle.innerHTML = "Devonian Period"
+  infoDivTime.innerHTML = "(419 to 359 million years ago)";
+  infoDivContent.innerHTML = "Armored fish.";
+}
+
+
+function carboniferousChange(){
+  var infoDivTitle = document.getElementById("infoDivTitle");
+  var infoDivContent = document.getElementById("infoDivContent");
+  infoDivTitle.innerHTML = "Carboniferous Period"
+  infoDivTime.innerHTML = "(359 to 299 million years ago)";
+  infoDivContent.innerHTML = "Lots of oxygen. Amphibians and reptiles appear. Large insects.";
+}
+
+function permianChange(){
+  var infoDivTitle = document.getElementById("infoDivTitle");
+  var infoDivContent = document.getElementById("infoDivContent");
+  infoDivTitle.innerHTML = "Permian Period"
+  infoDivTime.innerHTML = "(299 to 252 million years ago)";
+  infoDivContent.innerHTML = "Ends with Permian Extinction";
+}
+
+function paleogeneChange(){
+  var infoDivTitle = document.getElementById("infoDivTitle");
+  var infoDivContent = document.getElementById("infoDivContent");
+  infoDivTitle.innerHTML = "Paleogene Period"
+  infoDivTime.innerHTML = "(66 to 23 million years ago)";
+  infoDivContent.innerHTML = "Mammals start to evolve - cats, camels, bats, elephants, deer, dogs, and more. Birds also begin to diversify, into eagles, hawks, and flightless birds like penguins. A mammal called the artiodactyl begins evolving into whales.";
+}
+
+function neogeneChange(){
+  var infoDivTitle = document.getElementById("infoDivTitle");
+  var infoDivContent = document.getElementById("infoDivContent");
+  infoDivTitle.innerHTML = "Neogene Period"
+  infoDivTime.innerHTML = "(23 to 3 million years ago)";
+  infoDivContent.innerHTML = "During this time period, mammoths continue to diversify. We start to see hyenas, giraffes, hippopotamus, zebras, mammoths, and more. Most importantly (for us, at least), this is where we can find Australopithecus - the ancestor of modern humans. This is when one of humanity's most unique aspects evolve: walking on two legs instead of four.";
+}
+
+function quaternaryChange(){
+  var infoDivTitle = document.getElementById("infoDivTitle");
+  var infoDivContent = document.getElementById("infoDivContent");
+  infoDivTitle.innerHTML = "Quaternary Period"
+  infoDivTime.innerHTML = "(3 million years ago to present day)";
+  infoDivContent.innerHTML = "During this time period, our ancestors continued to evolve. Australopithecus diverged into Neanderthals, Homo Sapiens, Denisovans, and other human species. Eventually, Homo Sapiens become the dominant life form on Earth. Since then, humans have driven countless species to extinction, including the wooly mammoth.";
+}
