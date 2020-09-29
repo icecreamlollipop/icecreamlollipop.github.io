92b49930f4c14163eca87ab24bc85543cb2f33ad
icecreamlollipop <nicole.dong@gmail.com>
2020-09-28 18:50:44 -0600

    updates

--- style.js
+++ style.js
@@ -1,6 +1,13 @@
-function changeContent() {
+function changeToPortfolio() {
     var mydiv = document.getElementById("mainContent");
     mydiv.innerHTML = "";
+    var myList1 = document.getElementById("list1");
+    var myList2 = document.getElementById("list2");
+    myList1.innerHTML = "";
+    myList2.innerHTML = "";
+    var par2 = document.getElementById("para2");
+    par2.innerHTML="";
+
     var aTag = document.createElement('a');
     // TODO: change this to the correct html -- the one that's not local
     aTag.setAttribute('href',"https://icecreamlollipop.github.io/EarthTimeline.html");
